5ac42b75ac9302376bd4bacd1d182af179953474
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-05-24 18:26:19 +0530

    factor and prime code added

--- codes/Hello.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Hello{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-       int  s=  sc.nextInt();
-       System.out.println(s);
-    }
-}
