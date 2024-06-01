e25eb116b2cdc542d415a506881ec95108998fd3
pulkit1417 <gupta.pulkit2408@gmail.com>
2024-06-01 15:08:25 +0530

    rotation of an array program added

---
+++ codes/rotateArray.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class rotateArray {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        reverse(arr, 0, n - 1);
+        reverse(arr, 0, k - 1);
+        reverse(arr, k, n - 1);
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void reverse(int[] arr, int s, int e) {
+        while (s < e) {
+            int temp = arr[s];
+            arr[s] = arr[e];
+            arr[e] = temp;
+            s++;
+            e--;
+        }
+    }
+}
